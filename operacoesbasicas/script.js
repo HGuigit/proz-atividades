79828b5eb5d9bec77c38f3aacfb8bab4adb27a40
Guilherme de Souza (hguigit) <guilherme.silva@technosoftware.com.br>
2023-10-31 20:09:33 -0300

    Atividade nova

---
+++ operacoesbasicas/script.js
@@ -0,0 +1,9 @@
+function operacoesBasicas(num1, num2) {
+    console.log(`Soma: ${num1} + ${num2} = ${num1 + num2}`);
+    console.log(`Subtração: ${num1} - ${num2} = ${num1 - num2}`);
+    console.log(`Multiplicação: ${num1} × ${num2} = ${num1 * num2}`);
+    console.log(`Divisão: ${num1} / ${num2} = ${num1 / num2}`);
+}
+
+// Exemplo de uso da função
+operacoesBasicas(4, 5);
